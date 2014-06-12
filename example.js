49e490bd8fc0db5d9e0c02dbbc90e9acfb3554c0
维奇 <mankai.chenmk@alibaba-inc.com>
2014-06-12 17:06:49 +0800

    c

--- example.js
+++ example.js
@@ -3,6 +3,6 @@ var sshkeys = require('./index.js');
 sshkeys.exec({
 	api: 'https://gitlab.com/api/v3',
 	token: 'e7zpqXbpAz6xkmnQgJpb'
-}, function() {
+}, function(data, keyPath, newCreate) {
 	console.log(arguments);
 });
